a1c06cd2258622b1404c3b050a97970b7b02d92f
Alexander zur Bonsen <alexander.zur.bonsen@tngtech.com>
2022-02-15 21:17:49 +0100

    add proto grpc gen script

---
+++ gen_proto_grpc.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -eou
+
+PROJECT_HOME=/Users/azb/VSCProjects/bazelbox/appletree
+/Users/azb/.pyenv/versions/otel/bin/python3.8 -m grpc_tools.protoc -I$PROJECT_HOME --python_out=$PROJECT_HOME --grpc_python_out=$PROJECT_HOME appletree.proto
